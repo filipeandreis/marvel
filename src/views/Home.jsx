b492efb589e0e1cebd15b59c0fd9c2c7ec849ef3
unknown <filipe.andreis@live.com>
2021-08-12 22:40:30 -0300

    alterações design home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Layout from '../layouts/default'
-import { Row, Col, Card } from 'react-materialize'
 import api from '../services/api'
 import { Animated } from 'react-animated-css'
+import { Icon } from 'react-materialize'
+import Menu from '../components/Menu'
+import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
 	constructor() {
@@ -18,6 +18,7 @@ class Home extends React.Component {
 		document.title = 'Home - Clientes Cidades'
 
 		//this.getcities()
+		this.runAnimation()
 	}
 
 	async getcities() {
@@ -29,45 +30,56 @@ class Home extends React.Component {
 			})
 		}
 	}
+
+	runAnimation(e) {
+		const element = document.getElementById('animate')
+        
+		if (element) {
+			// reset the transition by...
+			if(e) {
+				e.preventDefault()
+			}
+
+			element.classList.remove('run-animation')
+
+			void element.offsetWidth
+
+			element.classList.add('run-animation')
+		}
+	}
     
 	render() {
-		const { cities } = this.state
 		return (
-			<Layout>
-				<header className='center-align'>
-					<Row className="margin-card">
-						<Animated animationIn="fadeIn" animationInDuration={300}>
-							{
-								cities ?
-									cities.map((city, key) => (
-										<Col
-											key={key}
-											s={12}
-											m={5}
-											l={3}
-										>
-											<Card
-												actions={[
-													<Link
-														key={1}
-														to={`city-clients/${Buffer.from(city.city).toString('base64')}`}
-													>
-                                                    Ver clientes
-													</Link>
-												]}
-											>
-												<h5>{city.city}</h5>
-												<p>Qtd: {city.count}</p>
-											</Card>
-										</Col>
-									))
-									:
-									null
-							}
-						</Animated>
-					</Row>
-				</header>
-			</Layout>
+			<div className="body-home">
+				<Menu />
+				<Animated animationIn="fadeIn" animationInDuration={300} style={{ overflow: 'hidden' }}>
+					<div className="wrapper run-animation" id="animate">
+						<div className="logo">
+
+							<span className="marvel">Marvel</span>
+							<span className="studios">Studios</span>
+
+							<div>
+								<Icon small onClick={(e) => this.runAnimation(e)} className="restart">replay</Icon>
+								<br/>
+								<br/>
+							</div>
+                            
+							<Animated animationIn="fadeInUpBig" animationInDuration={2000} animationInDelay={2000}>
+								<Link style={{color: 'white'}} to='/characters'>
+									<button className="btn-home">
+										<span className="btn__inner">
+											<span className="btn__slide" />
+											<span className="btn__content">Ver personagens</span>
+										</span>
+									</button>
+								</Link>
+							</Animated>
+						</div>
+					</div>
+					<div className="images"></div>
+				</Animated>
+			</div>
 		)
 	}
 }
